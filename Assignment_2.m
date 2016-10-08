9c45ba5fde37e86f3331d2a67dc152895aab4c57
Miriam Mikhelson <Miriam@dhcp-10-103-234-100.wireless.northwestern.private>
2016-10-08 11:44:32 -0500

    tweaks

--- Assignment_2.m
+++ Assignment_2.m
@@ -1,4 +1,16 @@
+high_gain_photos = dir('High Gain Photos');
+low_gain_photos = dir('Low Gain Photos');
+high_gain_pixel_vals = [];
+low_gain_pixel_vals = [];
+for i = 1:numel(high_gain_photos)
+    image = imread(i);
+    high_gain_pixel_vals[i] = image(500,1000);
+    
 
 
-t = Tiff('1.dng','r');
-im = read(t);
+
+% 
+% t = Tiff('1.dng','r');
+% im = read(t);
+
+im = imread('1.dng');
